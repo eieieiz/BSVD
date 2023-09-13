51f8ff04fb78ac735141c3e79d048c8676b00263
zhe <zhe.zheng@ens-paris-saclay.fr>
2023-09-13 14:53:54 +0200

    bash script for test

---
+++ test.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+# python ./run_test.py -opt ./options/bsvd_test.yml --noisy_input true    --read_std true > debug/IRdataset.txt
+python ./run_test.py -opt ./options/bsvd_test.yml > debug/IRdataset.txt
